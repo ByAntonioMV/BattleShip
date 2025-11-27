1324ca0d7e0a2ad812b963fb2c79dc88f9d09840
samuel-armas <rogeliosamuelarmasgarcia99@gmail.com>
2025-11-27 12:30:53 -0600

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Cargar configuración
+    app.config.from_object(Config)
+
+    # Inicializar base de datos
+    db.init_app(app)
+
+    # Registrar blueprints (rutas)
+    from app.routes import init_routes
+    init_routes(app)
+
+    return app
